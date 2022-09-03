084e16f11b034c33c1db474f78269983f6f72ea1
MD IMRAN MRIDHA <mdimranmridha.pro@gmail.com>
2022-09-03 21:28:56 +0600

    Update Blog Page

--- js/news.js
+++ js/news.js
@@ -111,7 +111,6 @@ const displayCategoryNews =  (categoryNews,name) => {
                     <button onclick="loadPostDetails('${_id}')" type="button" class="px-6 py-2.5 bg-red-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-red-700 hover:shadow-lg focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-800 active:shadow-lg transition duration-150 ease-in-out" data-bs-toggle="modal"data-bs-target="#exampleModal">
                     <i class="fa-solid fa-arrow-right"></i>
                     </button>
-
                   </div>
                 </div>
               </div>
